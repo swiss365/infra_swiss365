83dccc018b72d769bc9e1fb321d8c4cbf808dcf9
swiss365 <info@swiss365.cloud>
2025-07-31 16:04:23 +0200

    Add DNS automation

--- providers.tf
+++ providers.tf
@@ -9,9 +9,17 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5"
     }
+    hetznerdns = {
+      source  = "timohirt/hetznerdns"
+      version = ">= 2.0"
+    }
   }
 }
 
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+provider "hetznerdns" {
+  apitoken = var.hetznerdns_token
 }
