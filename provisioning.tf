4cad9dc223c37044d530fa242a82aaf07f44d2a0
swiss365 <info@swiss365.cloud>
2025-08-08 15:26:23 +0200

    Add Terraform provisioning for server services

---
+++ provisioning.tf
@@ -0,0 +1,53 @@
+resource "null_resource" "configure_servers" {
+  depends_on = [
+    module.control_node,
+    module.workspace_host,
+    module.desktop_pool_host,
+    module.guac_lb
+  ]
+
+  triggers = {
+    control_ip   = module.control_node.ipv4
+    workspace_ip = module.workspace_host.ipv4
+    desktop_ip   = module.desktop_pool_host.ipv4
+    control_pw   = random_password.control_pw.result
+    workspace_pw = random_password.workspace_pw.result
+    desktop_pw   = random_password.desktop_pool_pw.result
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+cat > ansible/inventory.yml <<'EOF_INVENTORY'
+all:
+  children:
+    control:
+      hosts:
+        control:
+          ansible_host: ${self.triggers.control_ip}
+          ansible_user: root
+          ansible_password: ${self.triggers.control_pw}
+          ansible_connection: ssh
+          guac_rdp_password: ${self.triggers.control_pw}
+    workspace:
+      hosts:
+        workspace:
+          ansible_host: ${self.triggers.workspace_ip}
+          ansible_user: root
+          ansible_password: ${self.triggers.workspace_pw}
+          ansible_connection: ssh
+          guac_rdp_password: ${self.triggers.workspace_pw}
+    desktop_pool:
+      hosts:
+        desktop_pool:
+          ansible_host: ${self.triggers.desktop_ip}
+          ansible_user: root
+          ansible_password: ${self.triggers.desktop_pw}
+          ansible_connection: ssh
+          guac_rdp_password: ${self.triggers.desktop_pw}
+EOF_INVENTORY
+
+ANSIBLE_CONFIG=ansible/ansible.cfg ansible-playbook ansible/site.yml
+EOT
+    working_dir = path.root
+  }
+}
