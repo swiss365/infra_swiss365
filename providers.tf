506b340821eb5768a60641e5c19ca26d4bd25c7c
swiss365 <info@swiss365.cloud>
2025-12-06 17:48:33 +0100

    Add Hetzner DNS provider configuration

--- providers.tf
+++ providers.tf
@@ -9,9 +9,17 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5"
     }
+    hetznerdns = {
+      source  = "timohirt/hetznerdns"
+      version = ">= 2.4"
+    }
   }
 }
 
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+provider "hetznerdns" {
+  apitoken = var.hetznerdns_api_token
 }
