d0e280205a3b5f8ce75b65d731539a55770d3223
swiss365 <info@swiss365.cloud>
2025-12-08 15:07:21 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,14 +1,13 @@
+# network.tf - Network configuration for Swiss365 infrastructure
+
 resource "hcloud_network" "swiss365_net" {
   name     = "${var.customer_id}-network"
-  ip_range = var.network_cidr
-  labels = {
-    customer = var.customer_id
-  }
+  ip_range = "10.0.0.0/16"
 }
 
 resource "hcloud_network_subnet" "swiss365_subnet" {
   network_id   = hcloud_network.swiss365_net.id
   type         = "cloud"
   network_zone = "eu-central"
-  ip_range     = "10.20.1.0/24"
+  ip_range     = "10.0.1.0/24"
 }
